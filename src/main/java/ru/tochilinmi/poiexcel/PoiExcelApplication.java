c1bbbac0bd2c01bb438327278cab25a256b20d2f
TochilinMI <tochilin.m.i@mail.ru>
2022-10-25 14:36:19 +0300

    Init commit

---
+++ src/main/java/ru/tochilinmi/poiexcel/PoiExcelApplication.java
@@ -0,0 +1,14 @@
+package ru.tochilinmi.poiexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoiExcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoiExcelApplication.class, args);
+
+    }
+
+}
